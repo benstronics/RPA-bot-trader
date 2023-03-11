3276ea274bae1463ed1c3f800e2ec0d1ccd7bfb8
BEN-HUR P B DOS SANTOS <benstronics@gmail.com>
2023-03-11 19:22:14 -0400

    home-11-03-2023

--- rpa.py
+++ rpa.py
@@ -103,6 +103,74 @@ def rpa_get_values_cotation(loop:bool=True,simbol:str='BTC/USD'):
     #input('digite')
     return lista
 
+def download_historical_data(simbol):
+    try:
+        os.mkdir('base')
+    except Exception as e:
+        logging.exception(e)
+
+    link = 'https://forexsb.com/historical-forex-data'
+    lista = []
+    caminho = os.path.abspath(os.getcwd() + '/base')
+    ref.delete_Files_Folder('base')
+    # configurando argumentos para inicialização do chrome
+    options = Options()
+    options.add_experimental_option("prefs", {
+        "download.default_directory": caminho,
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing.enabled": True,
+        "plugins.plugins_list": [{"enabled": False, "name": "Chrome PDF Viewer"}],
+        # Disable Chrome's PDF Viewer
+        "download.extensions_to_open": "applications/pdf",
+        'plugins.always_open_pdf_externally': True
+    })
+    headless = str(ref.criar_filtros([0], ['headless'], 'filtro_chromedriver_headless')['headless'][0]).strip()
+    if int(headless) == 1:
+        options.add_argument('headless')
+    chrome_service = Service(ChromeDriverManager(path=os.getcwd()).install())
+    chrome_service.creationflags = CREATE_NO_WINDOW
+    driver = webdriver.Chrome(service=chrome_service, options=options)
+    try:
+        driver.get(link)
+
+        # create an ActionChains object
+        actions = ActionChains(driver)
+
+        # Encontrando 1° iframe
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+            (By.TAG_NAME, 'iframe')))
+        iframe = driver.find_element(By.TAG_NAME, 'iframe')
+        print(iframe)
+        driver.switch_to.frame(iframe)
+
+        elemento = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'select-symbol')))
+        actions.double_click(elemento).perform()
+        actions.send_keys(Keys.DELETE).perform()
+        elemento.send_keys(simbol, Keys.ENTER)
+
+        elemento = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'select-format')))
+        elemento.click()
+
+        options = elemento.find_elements(By.TAG_NAME,'option')
+        for option in options:
+            att = str(option.text).lower()
+            if 'json' in att:
+                option.click()
+
+
+        elemento = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'btn-load-data')))
+        elemento.click()
+
+        elemento = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="table-acquisition"]/tr[2]/td[6]/a')))
+        elemento.click()
+        time.sleep(5)
+        ref.esperar_downloads(caminho)
+
+    except Exception as e:
+        logging.exception(e)
+        input()
+
+
 if __name__ == '__main__':
-    lista = rpa_get_values_cotation(False)
-    print(lista)
+    download_historical_data('eurusd')
